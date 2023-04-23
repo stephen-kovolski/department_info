83ae73533239fbdb09f253da856b889d9039dfc0
Stephen Kovolski <s.kovolski@gmail.com>
2023-04-22 23:05:43 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const app = express();
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+  });
+
+
+app.listen(PORT, console.log(`listening on port ${PORT}`)); 
