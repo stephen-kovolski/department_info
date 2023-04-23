321b0274bae56e4aeadb1f566aa21c55fe86df5e
Stephen Kovolski <s.kovolski@gmail.com>
2023-04-22 23:33:03 -0400

    added files

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const app = express();
 const fs = require('fs');
 const PORT = process.env.PORT || 3000;
+let newObject = {};
 
 
+const data = fs.readFileSync('./textFiles/2_employeedepartment.txt', 'utf-8');
 
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+
+    res.send(data);
   });
 
 
